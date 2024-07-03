e4c4ecdcabd5acd5b75f96e34b1725df49573d07
chengivan <chengoleivan@gmail.com>
2024-07-03 13:40:55 +0300

    Fetching data from DB just fine

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,8 +1,11 @@
 import { createClient } from '@/utils/supabase/server';
 
-export default async function Notes() {
+export default async function Todos() {
   const supabase = createClient();
   const { data: todos } = await supabase.from("todos").select();
+  
 
-  return <pre>{JSON.stringify(todos, null, 2)}</pre>
+  return <div>
+    <p>{todos && todos[0]["task"]}</p>
+  </div>
 }
