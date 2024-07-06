cf8df99b20f7e7bfce782b94e3457d8e34670440
chengivan <chengoleivan@gmail.com>
2024-07-06 15:29:24 +0300

    Revalidating dashboard on visit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,12 +3,17 @@ import Taskform from "../add-task/TaskForm";
 import Todos from "../todos/Todos";
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
+import { revalidatePath } from "next/cache";
 
 export default async function Dashboard() {
   const supabase = createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
+  if (user) {
+    revalidatePath("/dashboard");
+  }
 
   return (
     <div>
